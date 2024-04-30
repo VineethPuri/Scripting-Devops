e045a7f7eb94e514902d590781039776af0292ba
vineethpuri <vineethpuri2306@gmail.com>
2024-04-30 07:57:46 +0530

    varibles first file

---
+++ Varibles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Hi Shashi, I am starting devops course"
+echo "Hi Ram, I came to know it is wide course"
+echo "Hi Shashi, Yes it is a big course but having number of opertuneties"
+echo "HI Ram, You mean there is full of job opnings right?"
