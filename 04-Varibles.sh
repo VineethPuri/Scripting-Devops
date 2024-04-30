016b0eaec0b1c63b272dd1a2c84744d3548ed214
vineethpuri <vineethpuri2306@gmail.com>
2024-04-30 08:26:18 +0530

    different types of varible declaretion

---
+++ 04-Varibles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring varibles
+PERSON1=$1
+PERSON2=$2
+
+#reffering varibles
+echo "Hi $PERSON1:: I am starting devops course"
+echo "Hi $PERSON2:: I came to know it is wide course"
+echo "Hi $PERSON1:: Yes it is a big course but having number of opertuneties"
+echo "HI $PERSON2:: You mean there is full of job opnings right?"
