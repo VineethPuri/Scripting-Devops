3943946e1863de35e7f576a785e796ea95fb9def
vineethpuri <vineethpuri2306@gmail.com>
2024-05-01 07:50:21 +0530

    username and password

---
+++ 05-Varibles.sh
@@ -0,0 +1,11 @@
+#!/bin/Bash
+
+echo " please enter username:: "
+
+read USERNAME  #here username is varable
+
+echo " please enter password:: "
+
+read PASSWORD 
+
+echo " Username is: $USERNAME, Password is: $PASSWORD "
