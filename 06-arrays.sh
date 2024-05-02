5d8e7474365c519e8f898f758eea860ca6d9b8a6
vineethpuri <vineethpuri2306@gmail.com>
2024-05-02 07:40:31 +0530

    arrays1

---
+++ 06-arrays.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DJTILLU" "MANINFIRE")
+
+# size of above array is 3
+# indix count is 0,1,2
+
+# list always starts with 0
+
+echo " Fisrt movie is : ${MOVIES[1]}
+echo " Second movie is : ${MOVIES[2]}
+echo " thired movie is : ${MOVIES[3]}
+echo " MOVIES is : ${MOVIES[@]}
