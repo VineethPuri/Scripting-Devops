b59326eed279b138ab0c2bc9e29bd13d90dcabe7
vineethpuri <vineethpuri2306@gmail.com>
2024-05-09 08:58:19 +0530

    installation

---
+++ 09-installation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ USERID -ne 0]
+then
+    echo " please run this script with superuser "
+    exit 1 
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+   echo " MYSQL installation is failed "
+   exit 1
+else 
+   echo " Installation of MYSQL is success "
+fi
+
+dnf install git -y
+
+if [ $? -ne 0]
+then
+   echo " GIT installation is failed "
+else
+   echo " GIT installation is success "
+fi
