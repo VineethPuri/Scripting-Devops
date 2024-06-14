14294de804157d13742d90edb50e26a96105fa3f
vineethpuri <vineethpuri2306@gmail.com>
2024-06-14 10:06:09 +0530

    Packages Installed

--- 15-Install-packages.sh
+++ 15-Install-packages.sh
@@ -5,7 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
    echo "Please run the script with root access"
-   Exit 1 #Manully exit if error comes
+   exit 1 #Manully exit if error comes
 else
    echo "You are super user"
 fi
@@ -13,4 +13,10 @@ fi
 for i in $@
 do
   echo "Package need to install: $i"
+  dnf list installed $i
+  if [ $? -eq 0]
+  then
+      echo " $i already installed "
+   else
+      echo " $i need to be installed "
 done
